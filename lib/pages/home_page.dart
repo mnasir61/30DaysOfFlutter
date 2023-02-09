2d86ae999e8934059d87356aed60aa439b16cf38
Muhammad Nasir <nproarchitech.inc@gmail.com>
2023-02-09 15:34:13 +0500

    inital

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text("Home"),
+      ),
+      body: const Center(
+        child: Text(
+          "Flutter Day 1",
+          style: TextStyle(
+            color: Colors.blueAccent,
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+          ),
+        ),
+      ),
+    );
+  }
+}
