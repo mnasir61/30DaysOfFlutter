4a5c3e10995aef81ffd8c2c73d5af5efd56824cc
Muhammad Nasir <nproarchitech.inc@gmail.com>
2023-02-09 21:12:50 +0500

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main(){
 class MyApp extends StatelessWidget {
 @override
 Widget build(BuildContext context) {
-return MaterialApp(
+return const MaterialApp(
   title: "Flutter App",
    debugShowCheckedModeBanner: false,
   home:HomePage(),
