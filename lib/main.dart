8de30fe420acb8757fbafa67115c62cf9feff94d
mnasir61 <nproarchitect.inc@gmail.com>
2023-02-10 23:59:34 +0500

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/pages/home_page.dart';
-void main(){
-  runApp(MyApp(),);
+import 'package:untitled/pages/login_page.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-return const MaterialApp(
-  title: "Flutter App",
-   debugShowCheckedModeBanner: false,
-  home:HomePage(),
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter App",
+      debugShowCheckedModeBanner: false,
+      // home:HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      // initialRoute: "/Home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
+  }
 }
